2323f73031d9a7356c5efccbc37f0bf213d6817e
Thiago Holanda <unnamedd@gmail.com>
2020-09-08 03:00:19 +0200

    Add artists.json as a Resource

--- session-02/MusicBands/Package.swift
+++ session-02/MusicBands/Package.swift
@@ -14,7 +14,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MusicBands",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("Resources")
+            ]
+        ),
         .testTarget(
             name: "MusicBandsTests",
             dependencies: ["MusicBands"]),
